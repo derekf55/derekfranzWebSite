1c23d9c5bc5ca43e9846b75ab8e60e50201b0338
= <derekfran55@gmail.com>
2021-03-05 00:54:12 -0600

    Updated way homemanagement works

--- php/peopleHere.sql
+++ php/peopleHere.sql
@@ -1,6 +1,1 @@
-SELECT macToName.Name, WifiInfo.last_seen 
-FROM `WifiInfo` JOIN macToName ON macToName.MacAddress = WifiInfo.MacAddress 
-WHERE WifiInfo.last_seen > CURRENT_TIMESTAMP - INTERVAL 2 MINUTE 
-AND WifiInfo.Relevant = 1
-GROUP by Name
-HAVING MAX(WifiInfo.last_seen)
+SELECT Name FROM PeopleHere
