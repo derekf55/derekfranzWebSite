9253f3e3dd450f2e942dd3a6b91ff3ad508bc9f0
= <derekfran55@gmail.com>
2021-04-18 19:21:11 -0500

    Added defaults for alarm

--- php/set_alarm.php
+++ php/set_alarm.php
@@ -1,37 +1,51 @@
 <?php
     require 'connect.php';
-    if (isset($_POST['alarm'])){
-        $room = $_POST['Room'];
-        $alarm = mysqli_real_escape_string($conn,$_POST['alarm']);
-        $alarm = strip_tags($alarm);
-        #echo $_POST['alarm'];
-        $timezone = 'America/Chicago';
-        date_default_timezone_set($timezone);
-        $alarm = date("Y-m-d H:i:s", strtotime($alarm));
-        if ($alarm < date("Y-m-d H:i:s",$_SERVER['REQUEST_TIME'])){
-            $alarm = date("Y-m-d H:i:s", strtotime('+1 days',strtotime($alarm)));
-            echo $alarm;
-            echo $room;
-        }
+    $DEFAULT_ROOM = "Derek_Room";
 
-        if (empty($alarm)){
-            echo 'alarm empty';
-            exit();
-        }
-
-        $sql = "INSERT INTO alarm (alarmTime, Room) VALUES (?,?);";
-        $stmt = mysqli_stmt_init($conn);
-        if (!mysqli_stmt_prepare($stmt,$sql)){
-            echo "Something went wrong";
-            exit();
-        }
-        else{
-            mysqli_stmt_bind_param($stmt,"ss",$alarm,$room);
-            mysqli_stmt_execute($stmt);
-            header("Location: ../alarm.php?sucess=yes");
-            exit();
-        }
-
+    // Check for errors 
+    if (!isset($_POST['alarm']) || $_POST['alarm'] == ""){
+        header("Location: ../alarm.php?sucess=no");
+        exit();
     }
+
+    $room = $_POST['Room'];
+    $alarm = mysqli_real_escape_string($conn,$_POST['alarm']);
+    $alarm = strip_tags($alarm);
+    $room = mysqli_real_escape_string($conn, $room);
+    $room = strip_tags($room);
+    
+    if ($room == "Choose..."){
+        $room = $DEFAULT_ROOM;
+    }
+
+
+    $timezone = 'America/Chicago';
+    date_default_timezone_set($timezone);
+    $alarm = date("Y-m-d H:i:s", strtotime($alarm));
+    if ($alarm < date("Y-m-d H:i:s",$_SERVER['REQUEST_TIME'])){
+        $alarm = date("Y-m-d H:i:s", strtotime('+1 days',strtotime($alarm)));
+        echo $alarm;
+        echo $room;
+    }
+
+    if (empty($alarm)){
+        echo 'alarm empty';
+        exit();
+    }
+
+    $sql = "INSERT INTO alarm (alarmTime, Room) VALUES (?,?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql)){
+        echo "Something went wrong";
+        exit();
+    }
+    else{
+        mysqli_stmt_bind_param($stmt,"ss",$alarm,$room);
+        mysqli_stmt_execute($stmt);
+        header("Location: ../alarm.php?sucess=yes");
+        exit();
+    }
+
+    
 
 ?>
