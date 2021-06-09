21930a96d07f5b4749d44adfdbbe79fc3d0cadd0
= <derekfran55@gmail.com>
2021-06-09 15:10:18 -0500

    Fixed logout issue

--- php/header.php
+++ php/header.php
@@ -3,6 +3,32 @@
     session_start();
     include 'php/connect.php';
 
+    // THIS IS VERY INSECURE BUT BETTER THAN THE NO LOGIN
+        // IF this cookie is good
+        if (isset($_COOKIE['rememberme'])){
+          //echo '<h1> Cookie val is '.$_COOKIE['rememberme'].'<h1>';
+          if ($_COOKIE['rememberme'] != ' '){
+            //echo '<h1>We haere</h1>';
+          $sql = "SELECT UserType FROM userinfo WHERE userid = ?;";
+          $stmt = mysqli_stmt_init($conn);
+          if(!mysqli_stmt_prepare($stmt,$sql)){
+            echo 'cookie error deal with';
+            exit();
+          } else {
+            mysqli_stmt_bind_param($stmt,"s",$_COOKIE['rememberme']);
+            mysqli_stmt_execute($stmt);
+            $results = mysqli_stmt_get_result($stmt);
+            if ($row = mysqli_fetch_assoc($results)){
+              $_SESSION['userType'] = $row['UserType'];
+              $_SESSION['uid'] = $_COOKIE['rememberme'];
+            }
+          }
+        } else{
+          
+        }
+        }
+
+    
     if(isset($_SESSION['uid'])){
       $sql = "SELECT * FROM userinfo WHERE userid=?;";
       $stmt = mysqli_stmt_init($conn);
@@ -23,7 +49,11 @@
           if ($_SESSION['userType'] == 'banned'){
             header("Location: login.php?error=banned");
           }
-    } else {
+    } 
+      
+    
+    else {
+      
         header("Location: login.php");
         
     }
