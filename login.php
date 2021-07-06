6865a5e919411ecfded291e7faa81df4eb9aef3f
= <derekfran55@gmail.com>
2021-07-05 23:18:00 -0500

    Made work offline

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="jquery/bootstrap.min.css" crossorigin="anonymous">
 
     <link rel="apple-touch-icon" sizes="120x120" href="img/apple-touch-icon-120.png">
     <link rel="icon" type="image/png" sizes="192x192" href="img/icon-192.png">
@@ -126,8 +126,8 @@
   <p class="mt-5 mb-3 text-muted">© Derek Franz</p>
 </form>
 
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="jquery/jquery-3.4.1.slim.min.js"  crossorigin="anonymous"></script>
+    <script src="jquery/popper.min.js" crossorigin="anonymous"></script>
+    <script src="jquery/bootstrap.min.js" crossorigin="anonymous"></script>
     
 </body></html>
